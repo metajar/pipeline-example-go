2b146075a70f38e4dc8379d41b5d7cd97f064e58
Jared Henry <jarhenry@justin.tv>
2020-04-03 14:25:37 -0500

    Canary

--- main.go
+++ main.go
@@ -17,5 +17,5 @@ func helloHandler(w http.ResponseWriter, r *http.Request) {
 	if err != nil {
 		panic(err)
 	}
-	fmt.Fprint(w, fmt.Sprintf("<h1>%v</h1>", name))
+	fmt.Fprint(w, fmt.Sprintf("CANARY: <h1>%v</h1>", name))
 }
