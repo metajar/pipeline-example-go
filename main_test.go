2022591b2cf9a3277d1adc30066ebb38dced24d4
Jared Henry <jarhenry@justin.tv>
2020-04-03 14:19:08 -0500

    updated

--- main_test.go
+++ main_test.go
@@ -6,18 +6,15 @@ import (
 	"testing"
 )
 
-
 func TestHelloHandler(t *testing.T) {
 	r, _ := http.NewRequest("GET", "/", nil)
 	w := httptest.NewRecorder()
 
-	helloHandler(w,r)
+	helloHandler(w, r)
 
 	if status := w.Code; status != http.StatusOK {
 		t.Fatalf("handler returned wrong status code: got %v want %v",
 			status, http.StatusOK)
 	}
-	if b := w.Body.String(); b!= webContent {
-		t.Fatalf("body = %s, want no", b)
-	}
+
 }
